9441b616077da6055513fbc3f115c213ab94bbf3
ranjithkumar21055 <ranjithkumar21055@gmail.com>
2025-07-13 19:18:11 +0530

    env variables assigned and conf file created as a best practice to ensure stringification of env variables.

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+// Best practice to ensure the variables are in string format if they are not already in the .env file.
+
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default conf;
