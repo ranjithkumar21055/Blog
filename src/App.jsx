155af9f0f659ece1e18339f6db5476d04e8066a7
ranjithkumar21055 <ranjithkumar21055@gmail.com>
2025-07-15 23:03:05 +0530

    Home page conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-
-import './App.css'
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice'
+import Footer from './components/Footer/Footer'
+import  Header  from './components/Header/Header'
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Blog</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login(userData))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .catch((error) => {
+      console.error('User not found', error);
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  },[])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-900 text-sky-100">
+      <div className="w-full block">
+        <Header/>
+        <main>
+          Todo 
+          {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
